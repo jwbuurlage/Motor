c98b9ddf93e210b2726eeb79232998b10e871168
Jan-Willem Buurlage <willem@moteoflife.net>
2012-02-03 23:45:26 +0100

    merge conflict

--- src/MotorModelManager.cpp
+++ src/MotorModelManager.cpp
@@ -45,6 +45,7 @@ namespace Motor {
 		cubeModel->setMesh( MeshManager::getSingleton().getMesh("default") );
 		cubeModel->setMaterial( MaterialManager::getSingleton().getMaterial("default") );
 		addResource("default", cubeModel);
+<<<<<<< HEAD
         
         Model* sphereModel = new Model;
 		sphereModel->setMesh( MeshManager::getSingleton().getMesh("sphere") );
@@ -55,6 +56,8 @@ namespace Motor {
 		planeModel->setMesh( MeshManager::getSingleton().getMesh("plane") );
 		planeModel->setMaterial( MaterialManager::getSingleton().getMaterial("default") );
 		addResource("plane", planeModel);
+=======
+>>>>>>> 9117291af14eb1668182493b8d49252f6c4bdc9c
 	}
 
 }
